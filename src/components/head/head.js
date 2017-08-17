773083fb3cbef3550c1ce149e105624a589eadf0
fishmankkk <609745278@qq.com>
2017-08-17 22:02:12 +0800

    head

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,18 +1,33 @@
 import React, {Component, PropTypes} from 'react';
-import {Row, Col } from 'antd';
+import {Row, Col, Icon, Modal } from 'antd';
+import styles from './style/style.scss'
 import logo from '../../assets/image/mricowater.png'
+const confirm = Modal.confirm;
 export default class HeadDetail extends Component {
   render() {
+    function loginOut() {
+      confirm({
+        title: '退出?',
+        content: '确定退出MIRCOWATER系统吗',
+        onOk() {
+          this.props.history.push('/')
+        },
+        onCancel() {},
+      });
+    }
       return (
-        <Row type="flex" justify="end">
-          <Col span={4}>
-            <div className="head_img">
+        <Row type="flex" justify="end"  align="middle">
+          <Col span={1}>
+            <div className={styles.headImg}>
               <img src={logo}/>
             </div>
           </Col>
-          <Col span={4}>
-            <div className="login_out">
-              注销
+          <Col span={1}>
+            <span className="m_water_f-keyword_s">超级管理员</span>
+          </Col>
+          <Col span={2} className="m_water_align-center m_water_pull-center ">
+            <div className="m_water_cusor-point login_out m_water_font-m m_water_fc-red" onClick={loginOut}>
+              <Icon type="logout" />注销
             </div>
           </Col>
         </Row>
--- src/view/user_page/nav/index.js
+++ src/view/user_page/nav/index.js
@@ -39,12 +39,20 @@ class SiderDemo extends React.Component {
         </Sider>
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }}>
-            <Icon
+            <Row type="flex" justify="end">
+            <Col span={1}>
+              <Icon
               className="trigger"
               type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
               onClick={this.toggle}
             />
-            <HeadDetail/>
+            </Col>
+            <Col span={23}>
+              <HeadDetail/>
+            </Col>
+          </Row>
+            
+            
           </Header>
           <Content style={{ margin: '24px 16px', padding: 24, background: '#fff' }}>
             Content
